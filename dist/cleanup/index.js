22d82f03268dd9a022b268fe8eed9b17d4e278ea
ryoshindo <udoryo999@gmail.com>
2023-02-23 20:41:37 +0900

    chore: build

--- dist/cleanup/index.js
+++ dist/cleanup/index.js
@@ -23,13 +23,12 @@ async function cleanup() {
     // environment variables, so we overwrite the current value with an empty
     // string. The AWS CLI and AWS SDKs will behave correctly: they treat an
     // empty string value as if the environment variable does not exist.
-    core.exportVariable('AWS_ACCESS_KEY_ID', '');
-    core.exportVariable('AWS_SECRET_ACCESS_KEY', '');
-    core.exportVariable('AWS_SESSION_TOKEN', '');
-    core.exportVariable('AWS_DEFAULT_REGION', '');
-    core.exportVariable('AWS_REGION', '');
-  }
-  catch (error) {
+    core.exportVariable("AWS_ACCESS_KEY_ID", "");
+    core.exportVariable("AWS_SECRET_ACCESS_KEY", "");
+    core.exportVariable("AWS_SESSION_TOKEN", "");
+    core.exportVariable("AWS_DEFAULT_REGION", "");
+    core.exportVariable("AWS_REGION", "");
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
